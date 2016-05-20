462facda75e1c9baee071ba973fe7686cc64d7f4
Hossein Saniei <HosseinAgha@users.noreply.github.com>
2016-05-20 16:14:21 +0430

    Create a small boilerplate in App entry point to get started

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+// var person_cls = require("./js/personNumber.js");
+// var table_cls = require("./js/personNumber.js");
+// var score_cls = require("./js/personNumber.js");
+// this is ES2015 way of importing modules. I imported React and React.Component from react package
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import styles from './main.styl'
+
+class App extends Component {
+	render() {
+		console.log('hohohoho');
+		return(
+			<div className={styles.message}> hello!!! </div>
+		)
+	}
+};
+
+ReactDOM.render( <App key="main"/>, document.getElementById('main') );
