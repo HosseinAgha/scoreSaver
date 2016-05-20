9a1f9ece3861d613a0530391e523a8d2b9e70511
Hossein Saniei <HosseinAgha@users.noreply.github.com>
2016-05-20 16:12:52 +0430

    Add stylus loaders, Better loader config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-module.exports = config(process.env.NODE_ENV != "production")
+module.exports = config( process.env.NODE_ENV != "production" )
 
 function config(devMode) {
   var options = {
@@ -7,41 +7,42 @@ function config(devMode) {
       filename: "[name].bundle.js",
       publicPath: "/build/"
     },
+    devtool: "cheap-module-eval-source-map",
     module: {
       loaders: [
-        { test: /\.css$/, loader: "style!css" },
+        { loader: "style!css", test: /\.css$/, exclude: /node_modules/ },
         {
-            test: /\.jsx?$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel', // 'babel-loader' is also a legal name to reference
-            query: {
-            presets: ['es2015']
-            }
-        },
-        { 
-            test: /\.js$/, 
-            exclude: /node_modules/, 
-            loader: "babel", 
-            query:
-              {
-                presets:['react']
-              }
+          loader: 'babel', // 'babel-loader' is also a legal name to reference
+          test: /\.jsx?$/, // use babel both for js and jsx files
+          exclude: /node_modules/,
+          query: {
+            presets:['react', 'es2015']
+          }
         }
       ]
     }
   }
 
+  // Add configs dynamically based on dev and production mode
   if(devMode == true) {
+    options.module.loaders.push({
+      loader: 'style!css?module&sourceMap&localIdentName=[name]_[local]_[hash:base64:3]!stylus',
+      test: /\.styl$/
+    })
     options.entry = {
       app: [
         'webpack-dev-server/client?http://localhost:8081',
         'webpack/hot/only-dev-server',
-        './src/main.js'
+        './src/main.jsx'
       ]
     } 
   } else {
+    options.entry.loaders.push({
+      loader: 'style-loader!css-loader?module!stylus-loader',
+      test: /\.styl$/
+    })
     options.entry = {
-      app: './src/main.js'
+      app: './src/main.jsx'
     }
   }
 
